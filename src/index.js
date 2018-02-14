d99c9380c190a05f698703c450f1e2e70b6ca3b5
Jared Lunde <jared.lunde@gmail.com>
2018-02-14 01:08:29 -0700

    Now uses cached length

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ export default function juxt (...fns) {
   const fnsLength = fns.length
 
   return function (...args) {
-    if (fns.length === 2) {
-      // Most will probably be two functions long, so this is a nice performance
+    if (fnsLength === 2) {
+      // Most will probably be two functions, so this is a nice performance
       // trick to use (prevents entering a loop, pushing to array, etc.)
       return [fns[0](...args), fns[1](...args)]
     }
